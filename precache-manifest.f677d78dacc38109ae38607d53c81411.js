cbcefc98d663ed75b00bdcae3b814eb6080fadec
akshat chaturvedi <akshat8605@gmail.com>
2019-01-06 18:30:02 +0530

    done

---
+++ precache-manifest.f677d78dacc38109ae38607d53c81411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9fe27f30177c6a980cc",
+    "url": "/static/js/main.e9fe27f3.chunk.js"
+  },
+  {
+    "revision": "3f97867e7ea06f728968",
+    "url": "/static/js/1.3f97867e.chunk.js"
+  },
+  {
+    "revision": "e9fe27f30177c6a980cc",
+    "url": "/static/css/main.899b7a27.chunk.css"
+  },
+  {
+    "revision": "3f97867e7ea06f728968",
+    "url": "/static/css/1.a0e7b697.chunk.css"
+  },
+  {
+    "revision": "e2e2dcf3abf2d6be997cf91a6ac17cf5",
+    "url": "/index.html"
+  }
+];
